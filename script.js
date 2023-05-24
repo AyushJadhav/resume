4e25df4352a4f5adb51407b369ba3e531616115f
AyushJadhav <37294348+AyushJadhav@users.noreply.github.com>
2023-05-24 10:16:46 +0530

    added work-experience module

--- script.js
+++ script.js
@@ -2,3 +2,8 @@ $(document).ready(function() {
     $('.navbar').addClass('fade-in');
     $('.container').addClass('fade-in');
   });
+
+  function openTab(th)
+            {
+                window.open(th.name,'_blank');
+            }
